c5ec0b44b96879a7307c383410eed973ac5c773a
David112x <86davidx64@gmail.com>
2023-11-22 13:57:13 -0500

    Revert "msm: camera: Use boot clock for recording start time"
    Breaks video recording with Google Camera
    
    This reverts commit 5ae64c05add49c4096654c47b28d34a22f795171.

---
+++ cirrus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Before building the kernel, first we need to install a few packages on Cirrus CI and so we'll do that.
+pacman -Syu --needed --noconfirm wget base-devel xmlto inetutils bc cpio python-sphinx python-sphinx_rtd_theme graphviz imagemagick git python zip github-cli fortune-mod ccache jre8-openjdk
+# After installing the packages, we can move onto calling the build script.
+
+bash cirrus_build.sh
