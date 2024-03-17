5bb01e4e1f8a1e12fa441f386256a57c99d36c8f
fredslbn <138670465+fredslbn@users.noreply.github.com>
2024-03-17 21:07:14 +0700

    Update neutron.sh

--- neutron.sh
+++ neutron.sh
@@ -8,9 +8,6 @@
 # Specify Kernel Directory
 KERNEL_DIR="$(pwd)"
 
-pacman -Sy --needed --noconfirm archlinux-keyring
-pacman -Syyu --needed --noconfirm wget xmlto base-devel inetutils bc cpio python-sphinx python-sphinx_rtd_theme graphviz git python zip fortune-mod
-
 git submodule update --init --recursive --remote
 
 ##----------------------------------------------------------##
